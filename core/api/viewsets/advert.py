145b7ded764e1cb761b2a90102f4086b3a252dd7
Alexander Golovnya <algol.mail@gmail.com>
2021-03-08 22:31:35 +0700

    Add click creation.

--- core/api/viewsets/advert.py
+++ core/api/viewsets/advert.py
@@ -1,13 +1,17 @@
+from infi.clickhouse_orm import Database
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from django.contrib.auth.models import User
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from django.conf import settings
 from django.core.cache import caches
 
-from core.models import Advert
+from core.models import Advert, CustomUser
 from core.api.serializers import AdvertSerializer
+from clicks.models import Clicks
+
+db = Database('demo')
 
 
 class AdvertAPI(ModelViewSet):
@@ -29,22 +33,19 @@ class AdvertAPI(ModelViewSet):
 
     def retrieve(self, request, pk=None):
         obj = self.get_object()
-        ip = request.META['REMOTE_ADDR']
+
+        # TODO add IP to Clicks
+        # ip = request.META['REMOTE_ADDR']
         
         try:
-            user = User.objects.get(id=self.request.user)
+            user = CustomUser.objects.get(id=self.request.user)
         except:
-            user = User.objects.get(username='Anonymous')
+            user = CustomUser.objects.get(email='anonymous@anonymous.an')
 
-        # hit, created = Hit.objects.get_or_create(
-        #     ip=ip,
-        #     user=user,
-        #     hitcount=obj.hit_count,
-        #     session='sss',
-        #     user_agent='api',
-        # )
+        db.insert([
+            Clicks(
+                advert_id=obj.id, advert_owner_id=obj.owner_id, advert_city_id=obj.city_id, visitor_id=user.id
+            )
+        ])
 
-        # if created:
-        #     caches['advert'].clear()
-        #
         return super(AdvertAPI, self).retrieve(self, request, pk=None)
